eb19a2ceba3b873b7553b034f3b9893fd2f47844
Maksim Pestryakov <mistymarry@mail.ru>
2020-10-16 20:20:56 +0300

    Update GuessNumber

--- main.py
+++ main.py
@@ -3,22 +3,22 @@ import random
 x = random.randint(1, 100)
 num = 0
 count = 0
-
+print('I made a number from 1 to 100, try to guess it with 7 attempts')
 while True:
-    num = int(input('Я загадал число от 1 до 100, попробуй угадай его c 7 попыток!\n'))
+    num = int(input('Enter a number!\n'))
     count += 1
     if count == 7:
-        print('Тебе не повезло! Все попытки закончились!')
+        print("You're out of luck! All attempts are over!")
         break
     elif num == x:
-        print(f'Тебе повезло ты угадал число с {count} попытки!')
-        if input('Не хочешь сыграть еще? "y|n"\n') == 'y':
+        print(f'You were lucky you guessed the number with {count} attempts!')
+        if input('Do you want to play again? "y|n"\n') == 'y':
             x = random.randint(1, 100)
             count = 0
         else:
-            print('Спасибо за игру!')
+            print('Thank you for playing!')
             break
     elif num > x:
-        print('Мое число меньше')
+        print('My number is less than!')
     else:
-        print('Мое число больше!')
+        print('My number is higher!')
